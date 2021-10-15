47b94b506a7f708533177707de19629985b3cb84
m mamchur <mnkmamchur@hotmail.com>
2021-10-15 15:30:57 -0600

    web app setup with holidaycss menu

--- razor/ChinookApp/WebApp/Pages/BasicDataMovement.cshtml.cs
+++ razor/ChinookApp/WebApp/Pages/BasicDataMovement.cshtml.cs
@@ -9,8 +9,19 @@ namespace WebApp.Pages
 {
     public class BasicDataMovementModel : PageModel
     {
+        public string MyName;
         public void OnGet()
         {
+            Random rnd = new Random();
+            int oddeven = rnd.Next(0, 25);
+            if (oddeven % 2 == 0)
+            {
+                MyName = $"Matthew is {oddeven}";
+            }
+            else
+            {
+                MyName = null;
+            }
         }
     }
 }
