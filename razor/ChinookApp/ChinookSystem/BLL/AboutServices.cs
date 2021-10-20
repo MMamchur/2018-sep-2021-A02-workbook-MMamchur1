7887ccfc33646e737e15ded8bee506ff7ee9a554
m mamchur <mnkmamchur@hotmail.com>
2021-10-20 17:28:37 -0600

    Added to About Services

--- razor/ChinookApp/ChinookSystem/BLL/AboutServices.cs
+++ razor/ChinookApp/ChinookSystem/BLL/AboutServices.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using ChinookSystem.Models;
+using ChinookSystem.DAL;
+#endregion
+
 namespace ChinookSystem.BLL
 {
     public class AboutServices
     {
+        #region Constructor andDI variable setup
+        private readonly ChinookContext _context;
 
+        internal AboutServices(ChinookContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Queries
+        public DatabaseVersion GetDataVersion() 
+        {
+            DatabaseVersion results = _context.DbVersions
+                .Select(x => new DatabaseVersion
+                {
+                    Version = x.Major + "." + x.Minor + "." + x.Build,
+                    ReleaseDate = x.ReleaseDate
+                }
+                ).FirstOrDefault();
+            return results;
+        }
+        #endregion
     }
 }
